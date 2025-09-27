304f9068d28438d8a8de8200eb4226c8756aef8f
bridge71 <1415806690@qq.com>
2025-09-27 18:27:52 +0800

    update

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,33 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        let mut it = s.split(",");
+        let name = it.next();
+        if let Some(name) = name {
+            if name.len() == 0 {
+                return Err(ParsePersonError::NoName);
+            }
+        }
+        let age = it.next();
+        if let Some(age) = age {
+            match age.parse::<usize>() {
+                Ok(u) => {
+                    if it.count() > 0 {
+                        return Err(ParsePersonError::BadLen);
+                    }
+                    return Ok(Person {
+                        name: name.unwrap().to_string(),
+                        age: u,
+                    });
+                }
+                Err(x) => return Err(ParsePersonError::ParseInt(x)),
+            }
+        }
+
+        return Err(ParsePersonError::BadLen);
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -38,9 +36,26 @@ enum IntoColorError {
 // that correct RGB color values must be integers in the 0..=255 range.
 
 // Tuple implementation
+fn check(color: i16) -> bool {
+    if color >= 0 && color <= 255 {
+        true
+    } else {
+        false
+    }
+}
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        if check(red) && check(green) && check(blue) {
+            Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -48,6 +63,16 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let (red, green, blue) = (arr[0], arr[1], arr[2]);
+        if check(red) && check(green) && check(blue) {
+            Ok(Color {
+                red: red as u8,
+                green: green as u8,
+                blue: blue as u8,
+            })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -55,6 +80,20 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            let (red, green, blue) = (slice[0], slice[1], slice[2]);
+            if check(red) && check(green) && check(blue) {
+                Ok(Color {
+                    red: red as u8,
+                    green: green as u8,
+                    blue: blue as u8,
+                })
+            } else {
+                Err(IntoColorError::IntConversion)
+            }
+        }
     }
 }
 
